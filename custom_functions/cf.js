795a279c0628649cd0fca3cb470230fa3c55770d
Lyndsey Reed <ler2144@tc.columbia.edu>
2020-01-08 15:44:11 -0500

    working

--- custom_functions/cf.js
+++ custom_functions/cf.js
@@ -1,11 +1,17 @@
-function calculateBill() {
+
+
+function calculateBill(billAmount, taxRate) {
   console.log("Running Calulate Bill");
-  const total = 100 * 1.13;
+  const total = billAmount * 1 + taxRate;
   console.log(total);
   return total;
 }
 
-// const myTotal = calculateBill();
-console.log(`Your total is ${calculateBill()}`);
+const myTotal = calculateBill(100, 0.13);
+const myTotal2 = calculateBill(200, 0.13);
+console.log(myTotal, myTotal2);
+
+
+// console.log(`Your total is ${myTotal, myTotal2}`);
 
 
