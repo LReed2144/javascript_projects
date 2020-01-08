ccba933ea8b1200deeedc662c19f08c7477d8624
Lyndsey Reed <ler2144@tc.columbia.edu>
2020-01-08 15:28:18 -0500

    adding more practice

--- custom_functions/cf.js
+++ custom_functions/cf.js
@@ -1,1 +1,11 @@
-console.log('it works!');
+function calculateBill() {
+  console.log("Running Calulate Bill");
+  const total = 100 * 1.13;
+  console.log(total);
+  return total;
+}
+
+// const myTotal = calculateBill();
+console.log(`Your total is ${calculateBill()}`);
+
+
