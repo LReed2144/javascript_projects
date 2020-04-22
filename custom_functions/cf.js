3d0f51f4dc6753c0853b6648483a6e25273d21c0
Lyndsey Reed <ler2144@tc.columbia.edu>
2020-04-22 07:08:08 -0400

    changes

--- custom_functions/cf.js
+++ custom_functions/cf.js
@@ -1,28 +1,7 @@
-
-
-function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) {
-  console.log("Running Calulate Bill");
-  const total = billAmount + billAmount * taxRate + billAmount * tipRate;
-  return total;
+function doctorize(firstName) {
+  return `Dr. ${firsttName}`;
 }
-const lyndsTotal = 70;
-const lyndsTaxRate = 0.20;
-// const myTotal = calculateBill(lyndsTotal,
-//   lyndsTaxRate);
-
-
-function sayHiTo(firstName) {
-  return`Hello ${firstName}`;
-}
-
-// const greeting = sayHiTo("Lyndsey");
-// console.log(greeting);
-// console.log(`Your total is ${myTotal, myTotal2}`);
-
-function doctorize(name = 'Silly Goose') {
-  return `Dr. ${name}`;
-}
-
-
-const myBill4 = calculateBill(100, undefined, 0.2);
-console.log(myBill4);
+console.log(doctorize(lyndsey));
+ // const doctorize = function(firstName) {
+ //  return`Dr. ${firstName}`;
+ // };
